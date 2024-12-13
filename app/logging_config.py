e4ca74bdbd8c9a58e1db32406702e0cadd39b03e
Mahsa Maleki Nazari <malekmacronos.be@Mahsas-MacBook-Pro.local>
2024-12-13 16:01:59 +0100

    Updated log timezone

--- app/logging_config.py
+++ app/logging_config.py
@@ -1,23 +1,31 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+from datetime import datetime
+import pytz
+
+class CustomFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, timezone=None):
+        super().__init__(fmt, datefmt)
+        self.timezone = timezone
+
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.fromtimestamp(record.created, tz=self.timezone)
+        if datefmt:
+            return dt.strftime(datefmt)
+        return dt.strftime("%Y-%m-%d %H:%M:%S")  # Default readable format
 
 def configure_logging(app):
-    """Set up logging for the Flask app."""
     log_file = app.config["LOG_FILE"]
-
     log_dir = os.path.dirname(log_file)
-
-    # Ensure the directory exists
     os.makedirs(log_dir, exist_ok=True)
-
-    # Create the log file if it doesn't exist
     if not os.path.exists(log_file):
         open(log_file, "w").close()
 
     log_handler = RotatingFileHandler(log_file, maxBytes=100000, backupCount=10)
     log_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    timezone = pytz.timezone("Europe/Brussels")  # Set Brussels timezone
+    formatter = CustomFormatter('%(asctime)s - %(levelname)s - %(message)s', timezone=timezone)
     log_handler.setFormatter(formatter)
 
     if not app.logger.handlers:
@@ -25,4 +33,3 @@ def configure_logging(app):
         app.logger.setLevel(logging.INFO)
 
     app.logger.info("Logging initialized successfully.")
-
